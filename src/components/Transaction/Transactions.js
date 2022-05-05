264d768d783939021030ef9333fb1e6aaf6fa104
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-05-05 16:08:14 +1000

    Bug fix with sign up not logging user in

--- src/components/Transaction/Transactions.js
+++ src/components/Transaction/Transactions.js
@@ -29,26 +29,28 @@ const Transactions = (props) => {
       setFilteredMonth(selectedMonth);
   };
   
-  const filteredTransactions = props.items.filter((transaction) => {
-    if (filterOption === '-'){
-      return (new Date(transaction.date) )
-    } else {
-      if (filteredYear === "-" && filteredMonth === '-') {
-        return (new Date(transaction.date) )
-      } 
-      else if (filteredMonth === '-') {
-        return new Date(transaction.date).getFullYear().toString() === filteredYear; 
-      }
-      else {
-        return (new Date(transaction.date).getFullYear().toString() === filteredYear && 
-        new Date(transaction.date).getMonth().toString() === filteredMonth);
-      }
-    }
-  });
 
-  if (props.items.length === 0 && props) {
+  console.log(props.items.length)
+  if (props.items.length === 0) {
     return <div>Loading transactions</div>
   } else {
+    const filteredTransactions = props.items.filter((transaction) => {
+      if (filterOption === '-'){
+        return (new Date(transaction.date) )
+      } else {
+        if (filteredYear === "-" && filteredMonth === '-') {
+          return (new Date(transaction.date) )
+        } 
+        else if (filteredMonth === '-') {
+          return new Date(transaction.date).getFullYear().toString() === filteredYear; 
+        }
+        else {
+          return (new Date(transaction.date).getFullYear().toString() === filteredYear && 
+          new Date(transaction.date).getMonth().toString() === filteredMonth);
+        }
+      }
+    });
+
     return (
       <div>
         <div className='Transactions balance-chart'>

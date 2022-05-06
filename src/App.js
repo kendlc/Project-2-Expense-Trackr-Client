64d26127575d61461e9b558f32f504d506f52ba0
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-06 17:51:13 +1000

    Revert "remove console.log"
    This reverts commit 767e755dd55fc47f3903277dea036357c76da784.

--- src/App.js
+++ src/App.js
@@ -59,6 +59,7 @@ const App = () => {
     .then(jsonResponse => {
       if (jsonResponse.errors) {
         setSignupErrors(jsonResponse.errors);
+        console.log(jsonResponse.errors)
       }
       else {
         setUser(jsonResponse)
